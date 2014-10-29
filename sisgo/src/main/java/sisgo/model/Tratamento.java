e2dc440d739ae897e354889fbfc12429ebfae0d9
tiagocorcos@gmail.com <tiagocorcos@gmail.com@d5f7a93a-6ca6-6bc5-3091-e795078ef9d4>
2014-10-29 03:17:08 +0000

    -Finalizando CRUD de tratamento

--- sisgo/src/main/java/sisgo/model/Tratamento.java
+++ sisgo/src/main/java/sisgo/model/Tratamento.java
@@ -1,14 +1,19 @@
 package sisgo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="tratamento")
+@SequenceGenerator(sequenceName="tratamento_id_seq", name="seq")
 public class Tratamento {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="seq")
 	private Integer id;
 	private String nome;
 	private Double valor;
