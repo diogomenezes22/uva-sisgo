ec82f937becd0829dcef8357bd96dcd0b8df8eb0
tiagocorcos@gmail.com <tiagocorcos@gmail.com@d5f7a93a-6ca6-6bc5-3091-e795078ef9d4>
2014-10-29 02:39:08 +0000

    -Finalizando listagem de paciente

--- sisgo/src/main/java/sisgo/dao/PacienteDao.java
+++ sisgo/src/main/java/sisgo/dao/PacienteDao.java
@@ -1,8 +1,11 @@
 package sisgo.dao;
 
+import java.util.Collection;
+
 import javax.inject.Inject;
 
 import org.hibernate.Session;
+import org.hibernate.criterion.Order;
 
 import sisgo.model.Paciente;
 
@@ -11,8 +14,13 @@ public class PacienteDao {
 	@Inject
 	private Session session;
 	
-	public Paciente load(Integer id) {
+	public Paciente carregar(Integer id) {
 		return (Paciente) session.get(Paciente.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Collection<Paciente> listar() {
+		return (Collection<Paciente>) session.createCriteria(Paciente.class).addOrder(Order.asc("nome")).list();
 	}
 	
 	public void salvar(Paciente paciente) {
